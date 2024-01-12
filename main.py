be93dad7d4813cdbde9ae58b22ba761a37508260
Tabinka <tabicode@protonmail.com>
2024-01-12 22:46:37 +0100

    Add method for setting a budget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Kakeibo Budget Planner
 import csv
-import datetime 
+from datetime import datetime
 
 def add_expense():
     category = input("What category: ")
@@ -9,7 +9,7 @@ def add_expense():
     # Code to add an expense
     with open('file.csv', 'w+') as f:
         writer = csv.writer(f)
-        writer.writerow([category,name,money,datetime.datetime.now()])
+        writer.writerow([category,name,money,datetime.now()])
 
 def view_expenses():
     # Code to view expenses
@@ -18,8 +18,13 @@ def view_expenses():
         for row in reader:
             print(f"Category: {row[0]}, name: {row[1]}, how much: {row[2]}, date: {row[3]}")
 
-#def set_budget():
+def set_budget():
     # Code to set monthly budget
+    monthly_budget = input("What is your monthly budget? ")
+    budget_category = input("For what category? ")
+    with open('budget.csv', 'w+') as f:
+        writer = csv.writer(f)
+        writer.writerow([datetime.today().month, monthly_budget, budget_category])
 
 #def view_reports():
     # Code to generate and view reports
@@ -27,10 +32,12 @@ def view_expenses():
 def main_menu():
     print("1) Add expenses \n 2) View expenses \n 3) Set budget \n 4) View reports\n")
     user_input = input("What is your choice? ")
-    if(user_input == "1"):
+    if user_input == "1":
         add_expense()
-    elif(user_input == "2"):
+    elif user_input == "2":
         view_expenses()
+    elif user_input == "3":
+        set_budget()
 
 if __name__ == "__main__":
     main_menu()
